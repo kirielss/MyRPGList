a1655b04804f1c37630a0bb27993d0074834e437
skiriel <gabrieldllopes@gmail.com>
2023-04-04 20:36:33 -0300

    feat: DbSet & Constructor

---
+++ MyRPGList/Data/MyRpgListDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRPGList.Models;
+
+namespace MyRPGList.Data
+{
+    public class MyRpgListDbContext : DbContext
+    {
+        public MyRpgListDbContext(DbContextOptions<MyRpgListDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
