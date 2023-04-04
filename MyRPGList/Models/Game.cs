f9d44ff813259f22804479f0c546ee5dac246ce5
skiriel <gabrieldllopes@gmail.com>
2023-04-03 22:37:55 -0300

    feat: add gamescontroller validation

--- MyRPGList/Models/Game.cs
+++ MyRPGList/Models/Game.cs
@@ -1,10 +1,16 @@
-﻿namespace MyRPGList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRPGList.Models
 {
     public class Game
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter the name of the game")]
+        [MaxLength(100, ErrorMessage ="The name of the game cannot exceed 100 characters")]
         public string Name { get; set; }
-        public Dev Developer { get; set; }
+        [Required(ErrorMessage ="Please enter the name of the developer")]
+        [MaxLength(100, ErrorMessage = "The name of the developer cannot exceed 100 characters")]
+        public string Developer { get; set; }
         public string? Description { get; set; }
     }
 }
