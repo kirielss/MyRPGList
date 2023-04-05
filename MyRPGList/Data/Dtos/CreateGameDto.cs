547ee3c1bb5d6c9c94570c17394b147ed9bec041
skiriel <gabrieldllopes@gmail.com>
2023-04-04 22:42:18 -0300

    feat: DTO added

---
+++ MyRPGList/Data/Dtos/CreateGameDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRPGList.Data.DTOs
+{
+    public class CreateGameDto
+    {
+        [Required(ErrorMessage = "Please enter the name of the game")]
+        [StringLength(100, ErrorMessage = "The name of the game cannot exceed 100 characters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter the name of the developer")]
+        [StringLength(100, ErrorMessage = "The name of the developer cannot exceed 100 characters")]
+        public string Developer { get; set; }
+        public string? Description { get; set; }
+    }
+}
