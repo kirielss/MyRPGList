a279f75ab335b4bf2b97b155413a2a324a5b16dc
skiriel <gabrieldllopes@gmail.com>
2023-04-04 21:30:22 -0300

    feat: MySql ConnectionString

--- MyRPGList/Data/MyRPGListDbContext.cs
+++ MyRPGList/Data/MyRPGListDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyRPGList.Models;
 
 namespace MyRPGList.Data
 {
-    public class MyRPGListDbContext : DbContext
+    public class MyRpgListDbContext : DbContext
     {
+        public MyRpgListDbContext(DbContextOptions<MyRpgListDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
     }
 }
--- MyRPGList/Program.cs
+++ MyRPGList/Program.cs
@@ -1,35 +1,41 @@
-namespace MyRPGList
+using Microsoft.EntityFrameworkCore;
+using MyRPGList.Data;
+
+namespace MyRPGList;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("GameConnection");
+
+        builder.Services.AddDbContext<MyRpgListDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
